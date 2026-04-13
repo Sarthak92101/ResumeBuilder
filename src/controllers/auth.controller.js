8a89693aec8d20f458ab71f319684d7154210af4
Sarthak Sharma <sarthaksharma25702@gmail.com>
2026-04-13 13:57:54 +0530

    Authentication completed

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 const userModel = require("../models/user.model")
 const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
+const tokenBlacklistModel = require("../models/blacklist.model")
 /**
 * @name registerUserController
 * @description register a new user,expects username,email and password in the request body
@@ -88,9 +89,43 @@ async function loginUserController(req, res) {
   })
 }
 
+/**
+ * @name logoutUserController
+ * @description logout a user
+ * @access Public 
+ */
+
+async function logoutUserController(req,res){
+  const token=req.cookies.token;
+  if(token){
+await tokenBlacklistModel.create({token })
+  }
+  res.clearCookie("token")
+  res.status(200).json({
+    message:"User logged out successfully"
+  })
+}
+
+
+/**
+ * @name getmeController
+ * @description get user details 
+ * @access Public 
+ */
+
+async function getmeController(req,res){
+const user= await userModel.findById(req.user.id)
+res.status(200).json({
+  message:"User details",
+  user:{
+    id:user._id,
+    username:user.username,
+    email:user.email
+  }
+})
+}
 
 
 
 
-
-module.exports = { registerUserController ,loginUserController }
+module.exports = { registerUserController ,loginUserController,logoutUserController,getmeController }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,6 @@
 const {Router}=require('express')
 const authController=require("../controllers/auth.controller")
+const authMiddleware=require("../middlewares/auth.middleware")
 
 const authRouter= Router()
 
@@ -17,6 +18,22 @@ authRouter.post("/register",authController.registerUserController)
  */
 
 authRouter.post("/login",authController.loginUserController)
+
+/**
+ * @route GET/api/auth/logout
+ * @description Clear token from cookie and add token in blacklist
+ * @access Public 
+ */
+
+authRouter.get("/logout",authController.logoutUserController)
+
+
+/**
+ * @route GET/api/auth/me
+ * @description Get user details
+ * @access Private
+ */
+authRouter.get("/me",authMiddleware.authUser,authController.getmeController)
 
 
 module.exports=authRouter;
