8ace8c66d79b571b367740aa95aac3296a881b1c
Sarthak Sharma <sarthaksharma25702@gmail.com>
2026-04-15 13:55:06 +0530

    login register UI

--- Frontend/src/features/auth/pages/Register.jsx
+++ Frontend/src/features/auth/pages/Register.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
+import {useNavigate, Link} from 'react-router'
 
 const Register = () => {
-  return (
-    <div>
-      Register
+  const navigate=useNavigate()
+    const handleSubmit=(e)=>{
+    e.preventDefault()
+  }
+   return (
+   <main>
+    <div className="form-container">
+      <h1>Register</h1>
+
+      <form action="" onSubmit={handleSubmit}>
+         <div className="input-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" name="username" id="username" placeholder='Enter Username Here' />
+        </div>
+        <div className="input-group">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" placeholder='Enter Email Address' />
+        </div>
+
+          <div className="input-group">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="password" placeholder='Enter Password' />
+        </div> 
+
+        <button className='button primary-button'>Register</button>
+      </form>
+
+      <p>Already have an account? <Link to={"/login"}>Login</Link></p>
     </div>
+   </main>
   )
 }
 
