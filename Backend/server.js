a879a3df13b858d5f17c6182a059b62e3cfd6711
Sarthak Sharma <sarthaksharma25702@gmail.com>
2026-04-26 13:01:57 +0530

    Gemini Integrated

--- Backend/server.js
+++ Backend/server.js
@@ -2,9 +2,10 @@ require("dotenv").config()
 const app=require("./src/app")
 
 const connectToDB=require("./src/config/database")
+const invokeGeminiAi=require("./src/services/ai.service")
 
 connectToDB();
-
+invokeGeminiAi();
 app.listen(3000,()=>{
   console.log("Server is runnning on port 3000")
 })
