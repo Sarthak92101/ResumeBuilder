cfde017333a2ac6439b2349ff3aae41ce4eb98f1
Sarthak Sharma <sarthaksharma25702@gmail.com>
2026-05-28 17:03:50 +0530

    Backend

--- Backend/src/models/interviewReport.model.js
+++ Backend/src/models/interviewReport.model.js
@@ -117,7 +117,11 @@ const interviewReportSchema = new mongoose.Schema({
   technicalQuestions: [technicalQuestionSchema],
   behaviouralQuestions: [BehaviouralQuestionSchema],
   skillGaps: [skillGapSchema],
-  preparationPlan: [preparationPlanSchema]
+  preparationPlan: [preparationPlanSchema],
+  user:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"users"
+  }
 }, {
   timestamps: true
 })
---
+++ Backend/src/routes/interview.routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/auth.middleware');
+const interviewController = require('../controllers/interview.controller');
+const upload=require("../middlewares/file.middleware")
+
+const interviewRouter = express.Router();
+const router = Express.Router();  
+
+/**
+ * @Route POST /api/interview/
+ * @description  generate new interview report on the basis of resume and job description
+ * @access private
+ */
+interviewRouter.post("/",authMiddleware.authUser,upload.single("resume"),interviewController.generateInterviewReportController)
+
+module.exports = interviewRouter;
