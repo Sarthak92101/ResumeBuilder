0de5729ebf4ce9e5d5fbbf8d059c1a6828ecc732
SARTHAK SHARMA <141639566+Sarthak92101@users.noreply.github.com>
2026-05-31 12:58:59 +0530

    Update interview.api.js

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://resumebuilder-backend-aidf.onrender.com",
   withCredentials: true,
 })
 
