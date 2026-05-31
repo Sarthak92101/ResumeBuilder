f38403024b2a2797d9a71ea94ab2c12daf0f165b
SARTHAK SHARMA <141639566+Sarthak92101@users.noreply.github.com>
2026-05-31 12:59:22 +0530

    Update auth.api.js

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // ✅ FIXED
+  baseURL: "https://resumebuilder-backend-aidf.onrender.com", // ✅ FIXED
   withCredentials: true,
 });
 
